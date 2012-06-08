4abb1fac57e22f9c4c45f13bcf61037a40422922
Takaya Kobayashi <jigsaw@live.jp>
2012-06-08 12:43:00 +0900

    fix stdout.c

--- kue-chip2/stdout.c
+++ kue-chip2/stdout.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 void output_register_dump(const data *d) {
-	printf("ACC: %d IX: %d\nCF: %d VF: %d NF: %d ZF: %d\nIN: %d:%2x OUT: %d:%2x\n", d->acc, d->ix, ((d->flags&0x8)>>3), ((d->flags&0x4)>>2), ((d->flags&0x2)>>1), (d->flags&0x1), (d->in)->flag, (d->in)->bits, (d->out).flag, (d->out).bits);
+	printf("ACC: %.2x IX: %.2x\nCF: %.2x VF: %.2x NF: %.2x ZF: %.2x\nIN: %d:%.2x OUT: %d:%.2x\n", d->acc, d->ix, ((d->flags&0x8)>>3), ((d->flags&0x4)>>2), ((d->flags&0x2)>>1), (d->flags&0x1), (d->in)->flag, (d->in)->bits, (d->out).flag, (d->out).bits);
 }
 
 void output_memory_dump_line(const data *d, int a) {
@@ -35,16 +35,16 @@ void output_memory_at_all_adress(const data *d) {
 }
 
 void output_result(const data *d) {
-	printf("%2x ", d->obj_code[0]);
+	printf("%.2x ", d->obj_code[0]);
 	if(d->code_size == 2) {
-		printf("%2x ", d->obj_code[1]);
+		printf("%.2x ", d->obj_code[1]);
 	}
-	printf("%s [CPU: %hd PC: %d ACC: %d IX: %d CF: %d VF: %d NF: %d ZF: %d IN: %d:%2x OUT: %d:%2x ", d->mnemonic_code, d->cpuid, d->pc, d->acc, d->ix, ((d->flags&0x8)>>3), ((d->flags&0x4)>>2), ((d->flags&0x2)>>1), (d->flags&0x1), (d->in)->flag, (d->in)->bits, (d->out).flag, (d->out).bits);
+	printf("%s [CPU: %.2x PC: %.2x ACC: %.2x IX: %.2x CF: %.2x VF: %.2x NF: %.2x ZF: %.2x IN: %d:%.2x OUT: %d:%.2x ", d->mnemonic_code, d->cpuid, d->pc, d->acc, d->ix, ((d->flags&0x8)>>3), ((d->flags&0x4)>>2), ((d->flags&0x2)>>1), (d->flags&0x1), (d->in)->flag, (d->in)->bits, (d->out).flag, (d->out).bits);
 	if(d->memory_changed == 0) {
 		printf("]\n");
 	} else if(d->memory_changed == 1) {
-		printf("%2x: %2x -> %2x]\n", d->program_memory[(int)d->modified_addr], d->prev, d->now);
+		printf("%.2x: %.2x -> %.2x]\n", d->program_memory[(int)d->modified_addr], d->prev, d->now);
 	} else {
-		printf("%2x: %2x -> %2x]\n", d->data_memory[(int)d->modified_addr], d->prev, d->now);
+		printf("%.2x: %.2x -> %.2x]\n", d->data_memory[(int)d->modified_addr], d->prev, d->now);
 	}
 }
